5c4bbb5dc1996f58dee88c8635b7122f203524a2
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-01-13 02:59:19 +0300

    finished drag n drop

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,21 @@
     </button>
     <hr>
     <div class="row align-items-start">
-      <div class="cake col">
-        <div class="cake-layer cake-layer-biscuit"
-             v-for="layer in layers"
+      <div class="cake col"
+           v-on:drop="drop"
+           v-on:dragover="allowDrop"
+      >
+        <div class="cake-layer"
+             v-for="(layer, i) in layers"
+             v-bind:class="'cake-layer-' + layer.type"
+             v-bind:style="{height: layer.height * 10 + 'px'}"
+             v-bind:data-id="i"
+             v-on:click="mouseClick(layer, 'inc')"
+             v-on:click.right="mouseClick(layer, 'dec')"
+             v-on:contextmenu.prevent
+             v-on:dragstart="dragStart"
+             v-on:drag="drag"
+             draggable
         >
         </div>
       </div>
@@ -26,18 +38,39 @@
           </thead>
           <tbody>
           <tr
-            v-for="layer in layers"
+            v-for="(layer, i) in layers"
           >
-            <td>{{layer.type}}</td>
-            <td>{{layer.height}}</td>
-            <td>3</td>
+            <td>
+              <select class="form-select" v-model="layer.type">
+                <option v-bind:value="key"
+                        v-for="(lt, key) in layerTypes"
+                >
+                  {{ lt.label }}
+                </option>
+              </select>
+            </td>
+            <td><input
+              type="text"
+              class="form-control"
+              v-model.number="layer.height"
+            >
+            </td>
+            <td>
+              <button class="btn btn-danger"
+                      v-on:click="deleteLayer(layer, i)">
+                <span class="close-button"
+                      v-on:click="deleteLayer(layer, i)"
+                >
+                  &times;</span>
+              </button>
+            </td>
           </tr>
           </tbody>
         </table>
       </div>
     </div>
     <hr>
-    <div class="alert alert-success">Цена {{totalPrice}}</div>
+    <div class="price alert alert-success">Цена <strong>{{ calcPrice | moneyRub }}</strong> руб</div>
 
   </div>
 </template>
@@ -46,10 +79,10 @@
 export default {
   data() {
     return {
-      totalPrice: 0,
       layers: [],
       defaultType: 'biscuit',
       defaultHeight: 4,
+      draggingElement: null,
       layerTypes: {
         biscuit: {
           price: 50,
@@ -66,13 +99,53 @@ export default {
       }
     }
   },
-  computed: {},
+  computed: {
+    calcPrice() {
+      return this.layers.reduce((s, i) => s + (i.height * this.layerTypes[i.type].price), 0)
+    }
+  },
   methods: {
     addLayer() {
       this.layers.push({
         type: this.defaultType,
         height: this.defaultHeight
       })
+    },
+    deleteLayer(layer, i) {
+      //equivalent of this.layers.splice(i, 1)
+      this.$delete(this.layers, i)
+    },
+    mouseClick(layer, act) {
+      if (act === 'inc') {
+        layer.height += 1
+      } else if (act === 'dec') {
+        layer.height -= 1
+      }
+    },
+    drag(ev) {
+      ev.target.classList.add('dragging')
+    },
+    dragStart(event) {
+      this.draggingElement = event.target
+    },
+    drop(event) {
+      this.draggingElement.classList.remove('dragging')
+      const from = this.draggingElement.dataset.id
+      const to = event.target.dataset.id
+      const arr = this.layers
+
+      //arr.splice(from, 1, arr.splice(to, 1, arr[from])[0]);
+      arr.splice(to, 0, arr.splice(from, 1)[0]);
+
+    },
+    allowDrop(ev) {
+      ev.preventDefault()
+    }
+  },
+  filters: {
+    moneyRub(value) {
+      if (typeof value !== 'number') return value
+      return value.toFixed(2)
     }
   }
 
@@ -82,10 +155,8 @@ export default {
 <style scoped>
 .cake-layer {
   margin: 0 0px;
-  background-color: rgb(0, 192, 226);
-  position: relative;
   border: 1px solid transparent;
-  padding: 1rem;
+  transition: height 0.5s;
 }
 
 .cake-layer-biscuit {
@@ -98,6 +169,19 @@ export default {
 
 .cake-layer-cheese {
   background-image: url("assets/cheese.png");
+}
+
+.close-button {
+  cursor: pointer;
+}
+
+.price {
+  font-size: 26px;
+  margin-right: 40px;
+}
+
+.dragging {
+  opacity: 0.5;
 }
 
 </style>
